b2dacf384c5fc0422187463e58f46adc1842fd30
whiskey-oper <williamsti@wsc.school.nz>
2026-05-05 11:28:39 +1200

    Added a picture and a second one

--- Lesson1.Input_and_Output/ascii_art.py
+++ Lesson1.Input_and_Output/ascii_art.py
@@ -1,11 +1,18 @@
 ### ASCII Art ###
 # Create a picture using lines, spaces and/or some letters.
 # You'll need to use print multiple times to create your picture.
-
-
-
+print("  _______")
+print("/         \\")
+print("|  O   O  |")
+print("|    V    |")
+print(" \\_______/")
 
 # -------------------------
 
 # EXTENSION
 # Try to create a more complicated ASCII picture
+
+print ("     _^_______^_    ")
+print ("    |   .   .   |   ")
+print ("    <     -     >  ")
+print ("     -_________-"  )
