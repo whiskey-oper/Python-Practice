551981b07e851985033bd754caf100debd987dbd
whiskey-oper <williamsti@wsc.school.nz>
2026-05-05 11:13:19 +1200

    Added files and folder in VSCode

---
+++ Lesson1.Input_and_Output/ascii_art.py
@@ -0,0 +1,11 @@
+### ASCII Art ###
+# Create a picture using lines, spaces and/or some letters.
+# You'll need to use print multiple times to create your picture.
+
+
+
+
+# -------------------------
+
+# EXTENSION
+# Try to create a more complicated ASCII picture
---
+++ _main.py
@@ -0,0 +1,1 @@
+print("Money, Money")
