4b620673c85d8aef93afcaf17a35abd721294101
whiskey-oper <williamsti@wsc.school.nz>
2026-05-07 12:26:01 +1200

    ...

---
+++ Lesson.2.Data-Types_and_Variables/4.madlib.py
@@ -0,0 +1,25 @@
+# Create a short Madlib: Get input from your user (a bunch of words), 
+# then output a madlib using those words.
+
+# Ask user for a name and save it in a variable
+name = input("Wai")
+# Ask user for an animal and save it in a variable
+animal = input("shark")
+# Ask user for a colour and save it in a variable
+colour = input("blue")
+# Ask user for an object and save it in a variable
+object = ("apple")
+# Print your madlib using the 4 variables above.
+print("There was a person named", name)
+# ----------------------------
+
+# EXTENSION
+# Research about 'print formatting in python'. 
+# Use what you learn to rewrite your madlib into easier to read code.
+
+# ----------------------------
+
+# EXPERT (for those who already know some Python)
+# Create a randomised madlib game
+# GOAL: Just like above except...
+#       Write 4-6 different madlibs and randomise which one is output.
--- _main.py
+++ _main.py
@@ -7,3 +7,12 @@ print(5.0 / 5.0)
 print("5" * 5)
 
 print(5 - 5.0)
+
+
+name = "Mickey Mouse"
+print("His name is", name)
+
+name = "Mikey Mouse"
+age = 23
+print(name + " here is", age)
+
