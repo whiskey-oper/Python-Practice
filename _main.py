32ae459b2a2d48d2cb9ceff3be0fbc36f7417ee7
whiskey-oper <williamsti@wsc.school.nz>
2026-05-28 12:20:31 +1200

    still working on the country guessing game

--- _main.py
+++ _main.py
@@ -1,31 +1,6 @@
-# --- THE PASSWORD SECURITY CHECK ---
-
-# Section 1
-user_password = "password123"
-user_password= input("Enter password here >>> ")
-if user_password == "admin123":
-    print("Access Granted.")
-elif user_password== " ":
-    print("Access Denied. Wrong password.")
-elif user_password== "admin124":
-    print("Please try again.")
-
-# Section 2
-login_attempts = 3
-user_input= input("Please submit the number of attempts... ")
-if login_attempts > 5:
-    print("Your account is locked.")
-else: login_attempts < 5
-print("You have attempts remaining.")
-
-
-# Section 3
-password_length = 5
-NEW_passwordlogin: str= input("Create a new password. Make sure it's strong. ")
-
-if password_length < 8:
-    print("Password weak. Must be atleast more than 8 characters and not less, idiot.")
-elif password_length > 8:
-    print("Strong password")
-else:
-    input("You may leave. Thank you for passing security check.")
+guess = input('What’s the password?')
+print('Checking password is a match…')
+while guess != 'secret':
+    guess = input('Try again')
+print('Checking password is a match…')
+input('Welcome!')
