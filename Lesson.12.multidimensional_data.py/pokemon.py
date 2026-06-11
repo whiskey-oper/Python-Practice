6272720ffdb66608754a5977bd0aa042efd4acdd
whiskey-oper <williamsti@wsc.school.nz>
2026-06-11 12:27:33 +1200

    pokemon in progress

---
+++ Lesson.12.multidimensional_data.py/pokemon.py
@@ -0,0 +1,35 @@
+# =====================================================================
+# PROJECT: Pokemon
+# Create a battle program where you battle a random pokemon
+# =====================================================================
+
+# TODO Import random module
+pokemon = [
+    ["Blissey", 714]
+    ["Greninja", 348]
+    ["Charizard", 360]
+    ["Gengar", 324]
+    ]
+
+# Wild Pokemon
+# TODO Create a multidimensional list that holds 4 pokemon names and their max health (you choose)
+# User Pokemon
+attack_list = [
+    ["Hyper blast", 53]
+    ["Star throwers", 34]
+    ["Flaming burst", 29]
+    ["Shadow", 17]
+    ]
+# TODO Create a multidimensional list that holds 4 pokemon attacks and their different damage
+
+# TODO Create a variable to hold a randomised wild pokemon
+# TODO Create a current_health variable and set it to the max health of the random pokemon
+# TODO Tell the user what pokemon they're facing
+# TODO Create a while loop that continues until current health <= 0
+    # TODO Ask the user which attack they'd like to use (list all 4 options, numbered); save input
+    # TODO Use try except to ensure the user has input a number; if they didn't tell them so and then use 'continue' to restart the loop
+    # TODO Using the number, get the attack damage value and minus it from current health
+
+# TODO Tell the user they defeated the pokemon
+
+# ====================================================
